8912e86694f036d4257df8d044ba23f788cf88cc
Rinu Tresa <rinutresamj@gmail.com>
2023-05-16 11:45:14 +0530

    first commit

---
+++ websiteproject/websiteapp/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import Place, Team
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    obj1 = Team.objects.all()
+    return render(request,"index.html",{'result':obj,'result1':obj1})
+
+#
+# def demo1(request):
+#     obj1=Team.objects.all()
+#     return render(request,"index.html",{'result1':obj1})
